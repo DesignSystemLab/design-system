2f87ca9a44a1efe6e0c87741e1d22cb458e51d49
jiyeon <jy7828@gmail.com>
2023-01-31 20:01:13 +0900

    feat: 버튼 컴포넌트 구현

--- packages/button/src/button.tsx
+++ packages/button/src/button.tsx
@@ -1,5 +1,113 @@
-const Button = () => {
-    return <button>testButton</button>;
+/** @jsxImportSource @emotion/react */
+import variantPresetColors from '@jdesignlab/theme';
+import type {ColorToken, Color} from '@jdesignlab/theme';
+import {css} from '@emotion/react';
+import PropTypes from 'prop-types';
+
+type HEX = `#${string}`;
+type TButtonSize = 'sm' | 'md' | 'lg' | 'xl';
+type TButtonVariant = 'solid' | 'outline' | 'ghost' | 'link' | 'unstyled';
+interface IButtonProps {
+    variant?: TButtonVariant;
+    size?: TButtonSize;
+    color?: ColorToken;
+    children: string;
+}
+
+// utils
+const colorTokenParser = (color: ColorToken) => {
+    color = color || buttonDefaultColorToken;
+    const defaultVariant = 'base';
+    const baseColor = color.includes('-') ? color.split('-')[0] : color;
+    const colorVariant = color.includes('-') ? color.split('-')[1] : defaultVariant;
+    if (baseColor && variantPresetColors[baseColor]) {
+        const parsedColor = variantPresetColors[baseColor][colorVariant];
+        return parsedColor;
+    }
+};
+
+const buttonDefaultColorToken: ColorToken = 'teal-lighten3';
+const whiteish: HEX = colorTokenParser('grey-lighten5');
+
+const buttonSizeSet: TButtonSize[] = ['sm', 'md', 'lg', 'xl'];
+
+const buttonDefaultStyle = {
+    backgroundColor: colorTokenParser(buttonDefaultColorToken),
+    border: 'none',
+    borderRadius: '4px',
+    whiteSpace: 'nowrap',
+    cursor: 'pointer'
+};
+
+const buttonVariantStyle = (variant: TButtonVariant, color: ColorToken) => {
+    const parsedColor: HEX = colorTokenParser(color);
+    const switchBackground = {
+        background: 'none',
+        color: parsedColor
+    };
+
+    switch (variant) {
+        case 'outline':
+            return {
+                ...switchBackground,
+                border: `solid ${parsedColor} 1px`,
+                '&:hover': css({background: parsedColor, color: whiteish})
+            };
+            break;
+        case 'ghost':
+            return {
+                ...switchBackground,
+                '&:hover': css({background: parsedColor, color: whiteish})
+            };
+            break;
+        case 'link':
+            return {
+                ...switchBackground,
+                '&:hover': css({color: parsedColor, textDecoration: 'underline'})
+            };
+            break;
+        case 'unstyled':
+            return {
+                background: 'none'
+            };
+            break;
+        default: // solid
+            return {
+                backgroundColor: parsedColor,
+                color: whiteish
+            };
+            break;
+    }
+};
+
+const buttonSizeStyle = (size: TButtonSize | undefined) => {
+    const sizeIndex = buttonSizeSet.indexOf(size);
+    return {
+        height: (sizeIndex + 3) * 8,
+        fontSize: (sizeIndex + 6) * 2,
+        padding: `0 ${(sizeIndex + 3) * 4}px`
+    };
+};
+
+const Button = ({children, variant, size, color}: IButtonProps) => {
+    const buttonStyle = {
+        ...buttonDefaultStyle,
+        ...buttonVariantStyle(variant, color || buttonDefaultColorToken),
+        ...buttonSizeStyle(size)
+    };
+    return <button css={{...buttonStyle}}>{children}</button>;
+};
+
+Button.displayName = 'Button';
+Button.propTypes = {
+    variant: PropTypes.string,
+    size: PropTypes.oneOf(buttonSizeSet),
+    children: PropTypes.string.isRequired
+};
+Button.defaultProps = {
+    variant: 'solid',
+    size: 'md',
+    children: 'button'
 };
 
 export default Button;
--- packages/button/stories/button.stories.jsx
+++ packages/button/stories/button.stories.jsx
@@ -1,8 +1,86 @@
 import {Button} from '../src';
 
 export default {
-    title: 'test-button',
-    component: Button
+    title: 'Components/Button',
+    component: Button,
+    decorators: [
+        Story => (
+            <div style={{display: 'flex', gap: '5px'}}>
+                <Story />
+            </div>
+        )
+    ]
 };
 
-export const TestButton = () => <Button />;
+const argTypes = {
+    color: {
+        name: 'color',
+        type: {name: 'string', required: false},
+        description: 'button color',
+        control: 'text'
+    },
+    variant: {
+        name: 'variant',
+        type: {name: 'string', required: false},
+        description: 'button variant',
+        control: {
+            type: 'select',
+            options: ['solid', 'outline', 'ghost', 'link', 'unstyled']
+        }
+    },
+    children: {
+        name: 'children',
+        type: {name: 'string', required: false},
+        description: 'button label',
+        table: {
+            type: {summary: 'string'},
+            defaultValue: {summary: 'Button'}
+        },
+        control: 'text'
+    },
+    size: {
+        name: 'size',
+        type: {name: 'string', required: false},
+        description: 'button size',
+        control: {type: 'radio', options: ['sm', 'md', 'lg', 'xl']}
+    }
+};
+
+const Template = args => <Button {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+    children: 'button',
+    size: 'md'
+};
+Basic.argTypes = argTypes;
+
+export const WithColors = () => (
+    <>
+        <Button color="amber">blue</Button>
+        <Button color="lightGreen">lightGreen</Button>
+        <Button color="blue-darken1">blue-darken1</Button>
+        <Button color="red-lighten3">red-lighten3</Button>
+        <Button color="pink-accent2">pink-accent2</Button>
+        <Button>no color</Button>
+    </>
+);
+
+export const WithVariants = () => (
+    <>
+        <Button variant="solid">solid</Button>
+        <Button variant="outline">outline</Button>
+        <Button variant="ghost">ghost</Button>
+        <Button variant="link">link</Button>
+        <Button variant="unstyled">unstyled</Button>
+    </>
+);
+
+export const WithSizes = () => (
+    <>
+        <Button size="sm">Button</Button>
+        <Button size="md">Button</Button>
+        <Button size="lg">Button</Button>
+        <Button size="xl">Button</Button>
+    </>
+);
