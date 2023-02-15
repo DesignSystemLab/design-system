6ecd66386410751c1006517d59372595ab4d0040
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-02-16 02:55:50 +0900

    modify: undefined 타입 허용

--- packages/utils/src/event.ts
+++ packages/utils/src/event.ts
@@ -1,6 +1,6 @@
 import type { EventType } from './eventTypes';
 
-export const callHandler = <T extends (event: EventType) => void | undefined>(defaultHandler: T, handler: T) => {
+export const callHandler = <T extends (event: EventType) => void>(defaultHandler: T, handler: T | undefined) => {
     return (event: EventType) => {
         defaultHandler?.(event);
         handler?.(event);
