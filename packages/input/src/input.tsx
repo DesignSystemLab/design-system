7c553ca5eb5607bf280ec999be0dd49c8460def3
jiyeon <jy7828@gmail.com>
2023-02-15 23:06:58 +0900

    fix: input component type error 해결

--- packages/input/src/input.tsx
+++ packages/input/src/input.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource @emotion/react */
 import type { InputVariant, InputProps, InputRightProps, InputLabelProps, InputSize } from './input-types';
-import { getColorByToken, hexToRgba, ClearableIcon, VisibleIcon, UnvisibleIcon } from '@jdesignlab/theme';
 import { inputWrapperStyle, inputRightStyle, inputLabelStyle, inputVariantStyle, inputSizeStyle } from './input-style';
 import { useState, useId } from 'react';
 
@@ -34,7 +33,7 @@ const Label = (props: InputLabelProps) => {
 };
 
 const Right = (props: InputRightProps) => {
-    const { icon, children, setValue } = props;
+    const { icon, children } = props;
     const onClick = () => {
         alert('clearable 준비중');
     };
