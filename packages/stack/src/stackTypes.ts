ad2c0fd2d0daec15f4cb6ab3fcae4f723cd9c79f
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-02-17 01:13:58 +0900

    feat: add emotion as prop

--- packages/stack/src/stackTypes.ts
+++ packages/stack/src/stackTypes.ts
@@ -1,24 +1,22 @@
 /**
  * @todo: Will be add to 'utils' package
  */
-type StyleUnit = '%' | 'px' | 'em' | 'vh';
-export type Spacing = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | `${number}${StyleUnit}`;
+type StyleUnit = '%' | 'px' | 'em' | 'vh' | 'em' | 'vh';
+export type SpacingToken = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | `${number}${StyleUnit}`;
 export type FlexToken = 'start' | 'end' | 'center' | 'between' | 'around' | 'stretch';
+export type AsElement = React.ElementType;
 
 export type Direction = 'horizontal' | 'vertical';
-export type FlexMap = {
-    [key in FlexToken]: string;
-};
-export type SpacingMap = {
-    [key in Spacing]: string;
-};
+export type FlexMap = Record<FlexToken, string>;
+export type SpacingMap = Record<SpacingToken, string>;
 
 export interface StackProps {
-    children?: React.ReactNode;
-    spacing?: Spacing;
-    align?: FlexToken;
-    justify?: FlexToken;
-    direction?: Direction;
-    responsive?: boolean;
-    wrap?: boolean;
+  children?: React.ReactNode;
+  as?: AsElement;
+  spacing?: SpacingToken;
+  align?: FlexToken;
+  justify?: FlexToken;
+  direction?: Direction;
+  responsive?: boolean;
+  wrap?: boolean;
 }
