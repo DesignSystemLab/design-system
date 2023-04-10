8e784fcce1129f50879e204a1cea618e3fc850bd
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-04-10 23:43:59 +0900

    ➖ remove: git cache files

--- packages/card/src/components/cardBody.tsx
+++
@@ -1,9 +0,0 @@
-/** @jsxImportSource @emotion/react */
-
-import { useContext } from 'react';
-import { CardContext } from './cardContext';
-
-export const CardBody = (props: { children: React.ReactNode }) => {
-  const context = useContext(CardContext);
-  return <div css={context?.style.bodyStyle}>{props.children}</div>;
-};
--- packages/card/src/components/cardHeader.tsx
+++
@@ -1,5 +0,0 @@
-/** @jsxImportSource @emotion/react */
-
-export const CardHeader = (props: { children: React.ReactNode }) => {
-  return <header>{props.children}</header>;
-};
