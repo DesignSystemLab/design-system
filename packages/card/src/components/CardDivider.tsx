1e2160a01d7ce1971f0fc0aca681f93e4add1a08
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-04-15 17:49:40 +0900

    ♻️ refacotr: add htmlRole attribute

--- packages/card/src/components/CardDivider.tsx
+++ packages/card/src/components/CardDivider.tsx
@@ -1,9 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { useContext } from 'react';
+import { createClassVariant } from '@jdesignlab/theme';
 import createDividerStyle from '../styles/createDividerStyle';
 import { CardContext } from './CardContext';
 
 export const CardDivider = () => {
   const { direction } = useContext(CardContext).styleProps;
-  return <div css={createDividerStyle(direction)} />;
+  return (
+    <div
+      css={createDividerStyle(direction)}
+      role="presentation"
+      className={createClassVariant('card', 'content', 'divider')}
+    />
+  );
 };
