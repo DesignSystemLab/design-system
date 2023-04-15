ca6feca8285e569cc43179e43d50bfb428ef5078
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-04-15 23:00:40 +0900

    🚚 rename: component naming to PascalCase

--- packages/popover/src/components/popoverBody.tsx
+++
@@ -1,8 +0,0 @@
-/** @jsxImportSource @emotion/react */
-import { useContext } from 'react';
-import { PopoverContext } from './popoverContext';
-
-export const PopoverBody = (props: { children: React.ReactNode }) => {
-  const context = useContext(PopoverContext);
-  return <section css={context?.style.bodyStyleProps}>{props.children}</section>;
-};
