01fc5bece6899e7d684555a5d5672c0e6593587c
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-04-17 22:39:37 +0900

    ♻️ refactor: return emotion CSS directly instead of function call

--- packages/popover/src/styles/createOverlayStyle.ts
+++ packages/popover/src/styles/createOverlayStyle.ts
@@ -1,13 +1,11 @@
 import { css } from '@emotion/react';
 
-const createOverlayStyle = () => {
-  return css({
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100vw',
-    height: '100vh'
-  });
-};
+const createOverlayStyle = css({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100vw',
+  height: '100vh'
+});
 
 export default createOverlayStyle;
