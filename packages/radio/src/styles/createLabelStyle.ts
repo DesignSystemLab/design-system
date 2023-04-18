51a7f49a2bc286a2a7b3aa428736fefdc4e95d3e
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-04-18 15:22:09 +0900

    ✨feat: add CSS generator function for Radio component

---
+++ packages/radio/src/styles/createLabelStyle.ts
@@ -0,0 +1,44 @@
+import { css } from '@emotion/react';
+import { getColorByToken } from '@jdesignlab/theme';
+import type { ColorToken } from '@jdesignlab/theme';
+import type { RadioSize } from '../types';
+
+const createLabelStyle = (size: RadioSize, fontColor: ColorToken) => {
+  const labelStyle = css({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: `${getColorByToken(fontColor)}`,
+    boxSizing: 'border-box',
+    padding: 0,
+    whiteSpace: 'nowrap',
+    transition: 'border 0.5s ease-in-out',
+    '& span': {
+      verticalAlign: 'middle'
+    }
+  });
+
+  const labelFontSize = () => {
+    switch (size) {
+      case 'lg':
+        return css({
+          fontSize: '18px',
+          lineHeight: '-0.02em'
+        });
+      case 'md':
+        return css({
+          fontSize: '16px',
+          lineHeight: '-0.01em'
+        });
+      default:
+        return css({
+          fontSize: '14px',
+          lineHeight: '-0.01em'
+        });
+    }
+  };
+
+  return [labelStyle, labelFontSize()];
+};
+
+export default createLabelStyle;
