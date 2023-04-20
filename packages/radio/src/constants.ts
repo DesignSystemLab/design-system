2734a08d9e876c2cb073f10afb911df5f1a509ee
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-04-20 12:51:58 +0900

    🔧 modify: add type

--- packages/radio/src/constants.ts
+++ packages/radio/src/constants.ts
@@ -1,7 +1,8 @@
 import type { ColorToken } from '@jdesignlab/theme';
 
+const RADIO_ID_PREFIX: string = 'j-radio';
 const DEFAULT_COLOR: ColorToken = 'secondary-500';
 const DEFAULT_FONT_COLOR: ColorToken = 'grey-darken3';
 const DEFAULT_DISABLED_COLOR: ColorToken = 'grey-lighten3';
 
-export { DEFAULT_COLOR, DEFAULT_DISABLED_COLOR, DEFAULT_FONT_COLOR };
+export { RADIO_ID_PREFIX, DEFAULT_COLOR, DEFAULT_DISABLED_COLOR, DEFAULT_FONT_COLOR };
