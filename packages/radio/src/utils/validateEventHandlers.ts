6bf3b13842da7cd968f472dfcaff38611ec8460f
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-04-23 17:49:27 +0900

    🐛 fix: apply preventDefault() for disabled state even if no event attribute

--- packages/radio/src/utils/validateEventHandlers.ts
+++ packages/radio/src/utils/validateEventHandlers.ts
@@ -1,25 +1,51 @@
-import { SyntheticEvent } from 'react';
 import { callHandler } from '@jdesignlab/utils';
-import type { EventType } from '@jdesignlab/utils';
-import type { RadioProps } from '../types';
+import type { SyntheticEvent } from 'react';
+import type { RadioProps, RadioAttributes } from '../types';
 
-const validateEventHandlers = (isUnavailable: boolean, props: RadioProps) => {
-  const radioProps: { [key: string]: string | ((event: EventType) => void) } = {};
+const validateEventHandlers = (
+  isUnavailable: boolean,
+  props: RadioProps,
+  rootProps: RadioAttributes | null | undefined
+) => {
+  const radioProps: RadioAttributes = {};
 
   const preventEvent = (event: SyntheticEvent) => {
     event.preventDefault();
   };
 
-  Object.entries(props).forEach(prop => {
-    const [name, key] = prop;
-    if (typeof key === 'function' && isUnavailable) {
-      radioProps[name] = callHandler(preventEvent, key);
-      return;
+  const defaultSetPreventEvent = (radioProps: RadioAttributes, preventHandler: (event: SyntheticEvent) => void) => {
+    if (isUnavailable && !radioProps.onClick) {
+      radioProps.onClick = preventHandler;
     }
-    radioProps[name] = key;
+    return radioProps;
+  };
+
+  Object.entries(props).forEach(prop => {
+    const [key, value] = prop;
+    if (typeof value === 'function') {
+      if (rootProps && rootProps[key]) {
+        if (isUnavailable) {
+          const combineHandler = callHandler(rootProps[key], value);
+          radioProps[key] = callHandler(preventEvent, combineHandler);
+          return;
+        }
+        radioProps[key] = callHandler(value, rootProps[key]);
+        return;
+      }
+
+      if (isUnavailable) {
+        radioProps[key] = callHandler(preventEvent, value);
+        return;
+      }
+    }
+    radioProps[key] = value;
   });
 
-  return radioProps;
+  if (!radioProps['onclick']) {
+    radioProps;
+  }
+
+  return defaultSetPreventEvent(radioProps, preventEvent);
 };
 
 export default validateEventHandlers;
