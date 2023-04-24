59305c5e89084998e8744148618d54a616f73472
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-04-24 19:43:31 +0900

    ♻️ refoctor: add basic HTML attributes to radio and integrate with RadioGroup handlers

--- packages/radio/src/components/Radio.tsx
+++ packages/radio/src/components/Radio.tsx
@@ -1,8 +1,11 @@
 /** @jsxImportSource @emotion/react */
+import { useId, useContext } from 'react';
 import { createClassVariant } from '@jdesignlab/theme';
 import { getCoponentText } from '@jdesignlab/react-utils';
-import { RadioLabel } from './RadioLabel';
-import { useId } from 'react';
+import { RadioContext } from './RadioContext';
+import RadioGroup from './RadioGroup';
+import RadioLabel from './RadioLabel';
+import useRadio from '../hooks/useRadio';
 import { DEFAULT_COLOR, DEFAULT_DISABLED_COLOR, RADIO_ID_PREFIX } from '../constants';
 import validateEventHandlers from '../utils/validateEventHandlers';
 import createRadioStyle from '../styles/createRadioStyle';
@@ -10,33 +13,47 @@ import type { RadioProps } from '../types';
 
 export const Radio = (props: RadioProps) => {
   const {
+    id,
     children,
     className,
     color = DEFAULT_COLOR,
     size = 'md',
     readonly = false,
     disabled = false,
+    name = '',
     ...restProps
   } = props;
-
-  const radioProps = validateEventHandlers(!!readonly || !!disabled, restProps);
-  const radioId = props.id ?? `${RADIO_ID_PREFIX}-${useId()}`;
+  const radioContext = useContext(RadioContext);
+  const isUnavailable = !!readonly || !!disabled;
+  const validRadioProps = validateEventHandlers(isUnavailable, restProps, radioContext?.rootProps);
+  const { radioValue, handleChange } = useRadio(isUnavailable, validRadioProps, radioContext?.setValue);
+  const radioId = id ?? `${RADIO_ID_PREFIX}-${useId()}`;
   const radioClassName = className
     ? `${createClassVariant('radio', 'wrapper')} ${className}`
     : createClassVariant('radio', 'wrapper');
   const radioText = getCoponentText(children);
 
   return (
-    <div className={radioClassName}>
+    <div
+      className={radioClassName}
+      css={{ display: 'inline-block' }}
+      aria-checked={radioContext ? radioContext.value === props.value : radioValue === props.value}
+    >
       <RadioLabel id={radioId} size={size}>
         <input
           type="radio"
+          readOnly={readonly}
+          name={radioContext ? radioContext.name : name}
+          className={createClassVariant('radio', 'input', 'radio')}
           css={createRadioStyle(color, DEFAULT_DISABLED_COLOR, disabled, size)}
           disabled={disabled}
-          {...radioProps}
+          {...validRadioProps}
+          onChange={handleChange}
         />
         {radioText}
       </RadioLabel>
     </div>
   );
 };
+
+Radio.Group = RadioGroup;
