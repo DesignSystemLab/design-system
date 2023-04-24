c5d76a5b73291443a8c4f356fcb3120be913a7d9
jiyeon <jy7828@gmail.com>
2023-04-24 17:12:56 +0900

    ✨ feat: add props (aria-*, onChange etc...)

--- packages/checkbox/src/components/Checkbox.tsx
+++ packages/checkbox/src/components/Checkbox.tsx
@@ -1,15 +1,28 @@
 /** @jsxImportSource @emotion/react */
 import type { CheckboxProps } from '../types';
-import { useId } from 'react';
+import { useId, useState } from 'react';
+import { callHandler } from '@jdesignlab/utils';
 
 export const Checkbox = (props: CheckboxProps) => {
   const { children, checked, ...otherProps } = props;
   const id = useId();
+  const [checkedValue, setCheckedValue] = useState<boolean>(!!checked);
+  const onDefaultCheckboxChange = () => {
+    setCheckedValue(!checkedValue);
+  };
 
   return (
     <div>
-      <label htmlFor={id}>
-        <input type="checkbox" role="checkbox" id={id} {...otherProps} />
+      <label htmlFor={id} aria-labelledby={id}>
+        <input
+          type="checkbox"
+          role="checkbox"
+          id={id}
+          checked={checkedValue}
+          aria-checked={checkedValue}
+          onChange={callHandler(onDefaultCheckboxChange, props?.onChange)}
+          {...otherProps}
+        />
         <span>{children}</span>
       </label>
     </div>
