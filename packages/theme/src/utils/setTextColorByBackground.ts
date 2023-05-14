e93cb16fd728de157c0f6247cc25327e624bf8e0
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-05-14 23:29:39 +0900

    🔧 modify: change textcolor black

--- packages/theme/src/utils/setTextColorByBackground.ts
+++ packages/theme/src/utils/setTextColorByBackground.ts
@@ -1,13 +1,12 @@
 import { HEX } from '../types/color';
-import { preset } from '@jdesignlab/j-provider';
 
 export const setTextColorByBackground = (color: HEX | string) => {
   const hexValue = color.replace('#', '');
   const rgbValues = hexValue.match(/.{1,2}/g)?.map(value => parseInt(value, 16)) ?? [];
   const brightness = (rgbValues[0] + rgbValues[1] + rgbValues[2]) / 3;
   if (brightness < 200) {
-    return preset.dark.color.fontColor;
+    return '#000000';
   } else {
-    return preset.light.color.fontColor;
+    return '#000000';
   }
 };
