17f33ddff87fd12194c7959af596ffdf5394988c
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-05-27 02:41:05 +0900

    ✨ feat: improved to support react-hook-form library.

--- packages/textarea/src/components/TextareaContainer.tsx
+++ packages/textarea/src/components/TextareaContainer.tsx
@@ -1,36 +1,43 @@
 /** @jsxImportSource @emotion/react */
-import { useContext } from 'react';
+import { ForwardedRef, forwardRef, RefCallback, useContext } from 'react';
 import { TEXTAREA_PLACEHOLDER_COLOR } from '../constants';
 import { useSmart } from '../hooks/useSmart';
 import { createTextareaStyle } from '../styles/createTextareaStyle';
 import { createWrapperStyle } from '../styles/createWrapperStyle';
 import { TextareaContext } from './TextareaContext';
-import type { ReturnContext } from '../types';
 import { useTextarea } from '../hooks/useTextarea';
+import type { ReturnContext } from '../types';
 
-export const TextareaContainer = (props: { children: React.ReactNode }) => {
-  const { styleProps, textareaProps, textareaId } = useContext<ReturnContext>(TextareaContext);
-  const { onChange, ...attributesWithoutOnChange } = textareaProps;
-  const textareaStyle = createTextareaStyle(styleProps, TEXTAREA_PLACEHOLDER_COLOR);
-  const wrapperStyle = createWrapperStyle(styleProps.width);
-  const { textareaRef, handleChange } = useSmart(onChange, styleProps.resize);
-  const { getLabelId } = useTextarea();
-  const labelId = getLabelId(props.children);
+export const TextareaContainer = forwardRef(
+  (props: { children: React.ReactNode }, externalRef: ForwardedRef<any> | null) => {
+    const { styleProps, textareaProps, textareaId } = useContext<ReturnContext>(TextareaContext);
+    const { onChange, ...attributesWithoutOnChange } = textareaProps;
+    const textareaStyle = createTextareaStyle(styleProps, TEXTAREA_PLACEHOLDER_COLOR);
+    const wrapperStyle = createWrapperStyle(styleProps.width);
+    const { textareaRef, handleChange } = useSmart(onChange, styleProps.resize);
+    const { getLabelId } = useTextarea();
+    const labelId = getLabelId(props.children);
 
-  return (
-    <div css={wrapperStyle}>
-      {props.children}
-      <textarea
-        role="textbox"
-        aria-multiline="true"
-        id={textareaId}
-        css={textareaStyle}
-        ref={textareaRef}
-        onChange={handleChange()}
-        {...(textareaProps.placeholder && { 'aria-placeholder': textareaProps.placeholder })}
-        {...(labelId && { 'aria-labelledby': labelId })}
-        {...attributesWithoutOnChange}
-      />
-    </div>
-  );
-};
+    return (
+      <div css={wrapperStyle}>
+        {props.children}
+        <textarea
+          role="textbox"
+          aria-multiline="true"
+          id={textareaId}
+          css={textareaStyle}
+          ref={e => {
+            if (typeof externalRef === 'function') {
+              externalRef(e);
+            }
+            textareaRef.current = e;
+          }}
+          onChange={handleChange()}
+          {...(textareaProps.placeholder && { 'aria-placeholder': textareaProps.placeholder })}
+          {...(labelId && { 'aria-labelledby': labelId })}
+          {...attributesWithoutOnChange}
+        />
+      </div>
+    );
+  }
+);
