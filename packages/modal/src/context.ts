ba259f5b39c19a328e19ee420a57eec6b6085bd5
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-05-27 19:00:22 +0900

    🔧 modify: Change the modal open and close interfaces. #59

--- packages/modal/src/context.ts
+++ packages/modal/src/context.ts
@@ -1,6 +1,19 @@
-import { createContext } from 'react';
-export const ModalContext = createContext({
+import { createContext, SetStateAction } from 'react';
+import { Dispatch } from 'react';
+
+interface ReturnContext {
+  open: boolean;
+  isOpen?: boolean;
+  hasCloseIcon?: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  onOpen?: () => void;
+  onClose?: () => void;
+}
+export const ModalContext = createContext<ReturnContext>({
   open: false,
-  setOpen: (value: boolean) => {},
-  hasCloseIcon: true
+  isOpen: false,
+  setOpen: () => {},
+  hasCloseIcon: true,
+  onOpen: () => {},
+  onClose: () => {}
 });
