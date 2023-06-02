1a12098c223a27d552fd308845c5591656586133
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-06-03 03:38:00 +0900

    ♻️ refactor: separation of common style values ​​through constants

---
+++ packages/select/src/constants.ts
@@ -0,0 +1,8 @@
+import { ColorToken } from '@jdesignlab/theme';
+
+const FONT_COLOR: ColorToken = 'font';
+const BORDER_COLOR: ColorToken = 'border';
+const DISABLED_COLOR: ColorToken = 'disabled';
+const BORDER_RADIUS: number = 4;
+
+export { FONT_COLOR, BORDER_COLOR, BORDER_RADIUS, DISABLED_COLOR };
