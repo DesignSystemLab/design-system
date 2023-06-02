5cedc5e6665bedcb6b64366333bc1851a321a12e
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-06-03 03:54:51 +0900

    🐛 fix: black color removal and colors token utilization

--- packages/theme/src/utils/setTextColorByBackground.ts
+++ packages/theme/src/utils/setTextColorByBackground.ts
@@ -1,12 +1,13 @@
-import { HEX } from '../types/color';
+import { variantPresetColors } from '../token/colors';
+import type { HEX } from '../types/color';
 
 export const setTextColorByBackground = (color: HEX | string) => {
   const hexValue = color.replace('#', '');
   const rgbValues = hexValue.match(/.{1,2}/g)?.map(value => parseInt(value, 16)) ?? [];
   const brightness = (rgbValues[0] + rgbValues[1] + rgbValues[2]) / 3;
   if (brightness < 200) {
-    return '#000000';
+    return variantPresetColors.font;
   } else {
-    return '#000000';
+    return variantPresetColors.grey.lighten4;
   }
 };
