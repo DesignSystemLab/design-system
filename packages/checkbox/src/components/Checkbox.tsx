fb033088353a857450d343d9feb383824756ab2f
jiyeon <jy7828@gmail.com>
2023-06-16 18:05:23 +0900

    ✨ feat: support react-hook-form (checkbox)

--- packages/checkbox/src/components/Checkbox.tsx
+++ packages/checkbox/src/components/Checkbox.tsx
@@ -1,69 +1,71 @@
 /** @jsxImportSource @emotion/react */
-import { callHandler } from '@jdesignlab/utils';
 import { createClassVariant } from '@jdesignlab/theme';
 import { ThemeContext } from '@jdesignlab/j-provider';
 import type { CheckboxProps } from '../types';
-import { useId, useState, useRef, useContext, useEffect } from 'react';
+import { useId, useRef, useContext, useEffect, forwardRef } from 'react';
 import { CheckboxGroup } from './CheckboxGroup';
 import { CheckboxGroupContext } from '../context';
 import { checkboxWrapperStyle, checkboxInputStyle, checkboxLabelStyle } from '../styles';
 import { useKeyboardHandler } from '../hooks/useKeyboardHandler';
 
-export const Checkbox = (props: CheckboxProps) => {
-  const { children, checked, value, color, readOnly, ...otherProps } = props;
-  const [checkedState, setCheckedState] = useState<boolean>(!!checked);
-  const { defaultValues } = useContext(CheckboxGroupContext); // Checkbox.Group 없으면 undefined
-  const themePreset = useContext(ThemeContext);
-  const checkboxRef = useRef(null);
-  const id = useId();
+type ExtendedInputProps = CheckboxProps & { Group?: typeof CheckboxGroup };
+export const Checkbox = Object.assign(
+  forwardRef<HTMLInputElement, ExtendedInputProps>((props: CheckboxProps, extendRef) => {
+    const { children, value, color, readOnly, ...otherProps } = props;
+    const { defaultValues } = useContext(CheckboxGroupContext);
+    const themePreset = useContext(ThemeContext);
+    const checkboxRef = useRef<HTMLInputElement | null>(null);
+    const id = useId();
 
-  const onCheckboxDefaultChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (readOnly === true) return;
-    setCheckedState(prev => !prev);
-  };
+    const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (!checkboxRef.current) return;
+      useKeyboardHandler({
+        event,
+        parentScope: '[role="group"]',
+        selectorOfList: 'input[type="checkbox"]'
+      });
+    };
 
-  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (!checkboxRef.current) return;
-    useKeyboardHandler({
-      event,
-      parentScope: '[role="group"]',
-      selectorOfList: 'input[type="checkbox"]',
-      setState: setCheckedState
-    });
-  };
+    useEffect(() => {
+      if (defaultValues && value && checkboxRef.current) {
+        const initialCheck = defaultValues.some(x => x === value);
+        if (initialCheck) {
+          checkboxRef.current.checked = initialCheck;
+        }
+      }
+    }, [defaultValues, checkboxRef]);
 
-  useEffect(() => {
-    if (defaultValues && value) {
-      setCheckedState(defaultValues.some(x => x === value));
-    }
-  }, [defaultValues]);
-
-  return (
-    <label
-      htmlFor={id}
-      aria-labelledby={id}
-      className={createClassVariant('checkbox', 'wrapper')}
-      css={checkboxWrapperStyle}
-    >
-      <input
-        type="checkbox"
-        role="checkbox"
-        id={id}
-        className={createClassVariant('checkbox', 'input')}
-        css={checkboxInputStyle(themePreset, color)}
-        ref={checkboxRef}
-        checked={checkedState}
-        aria-checked={checkedState}
-        onChange={callHandler(onCheckboxDefaultChange, props?.onChange)}
-        onKeyDown={e => onKeyDown(e)}
-        {...otherProps}
-      />
-      <span className={createClassVariant('checkbox', 'label')} css={checkboxLabelStyle}>
-        {children}
-      </span>
-    </label>
-  );
-};
-
-Checkbox.displayName = 'Checkbox';
-Checkbox.Group = CheckboxGroup;
+    return (
+      <label
+        htmlFor={id}
+        aria-labelledby={id}
+        className={createClassVariant('checkbox', 'wrapper')}
+        css={checkboxWrapperStyle}
+      >
+        <input
+          type="checkbox"
+          role="checkbox"
+          id={id}
+          className={createClassVariant('checkbox', 'input')}
+          css={checkboxInputStyle(themePreset, color)}
+          ref={e => {
+            if (typeof extendRef === 'function') {
+              extendRef(e);
+            }
+            checkboxRef.current = e;
+          }}
+          onChange={props?.onChange}
+          onKeyDown={e => onKeyDown(e)}
+          {...otherProps}
+        />
+        <span className={createClassVariant('checkbox', 'label')} css={checkboxLabelStyle}>
+          {children}
+        </span>
+      </label>
+    );
+  }),
+  {
+    displayName: 'Checkbox',
+    Group: CheckboxGroup
+  }
+);
--- packages/checkbox/src/components/CheckboxGroup.tsx
+++ packages/checkbox/src/components/CheckboxGroup.tsx
@@ -1,13 +1,10 @@
 /** @jsxImportSource @emotion/react */
-import { filterComponent } from '@jdesignlab/react-utils';
 import { createClassVariant } from '@jdesignlab/theme';
-import { Checkbox } from './Checkbox';
 import { CheckboxGroupContext } from '../context';
 import type { CheckboxGroupProps } from '../types';
 
 export const CheckboxGroup = (props: CheckboxGroupProps) => {
   const { children, defaultValue, ...otherProps } = props;
-  const checkboxChildren = filterComponent(children, Checkbox, true);
   const contextValue = {
     defaultValues: props.defaultValue ?? []
   };
@@ -15,7 +12,7 @@ export const CheckboxGroup = (props: CheckboxGroupProps) => {
   return (
     <CheckboxGroupContext.Provider value={contextValue}>
       <div role="group" className={createClassVariant('checkbox', 'group')} {...otherProps}>
-        {checkboxChildren}
+        {children}
       </div>
     </CheckboxGroupContext.Provider>
   );
