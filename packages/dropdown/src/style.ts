4b9ca81d0c6403ed56ebf2345507042a14cc95cf
jiyeon <jy7828@gmail.com>
2023-07-23 05:06:47 +0900

    🔧 modify: change kebab-case to camel-case

--- packages/dropdown/src/style.ts
+++ packages/dropdown/src/style.ts
@@ -89,7 +89,7 @@ export const dropdownLocationStyle = (
     padding: '5px',
     boxShadow:
       'rgba(0, 0, 0, 0.2) 0px 2px 4px -1px, rgba(0, 0, 0, 0.14) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 10px 0px',
-    'flex-direction': 'column', // TODO <ul></ul>??
+    flexDirection: 'column', // TODO <ul></ul>??
     '&.menu_open': {
       display: 'flex'
     },
