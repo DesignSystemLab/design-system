255505185539f4c8a2e670e9b8707e408590e73b
jiyeon <jy7828@gmail.com>
2023-08-09 03:54:08 +0900

    ➖ remove: unused property

--- packages/tabs/src/components/TabContent.tsx
+++ packages/tabs/src/components/TabContent.tsx
@@ -8,8 +8,8 @@ import { createClassVariant } from '@jdesignlab/theme';
 
 const Content = (props: TabsContentProps) => {
   const { children, value, ...otherProps } = props;
-  const { selectedTab, variant, lazy, accentColor } = useContext(TabsContext);
-  const style = { ...tabContentStyle(variant, lazy, accentColor) };
+  const { selectedTab, variant, lazy, accentColor, baseColor } = useContext(TabsContext);
+  const style = { ...tabContentStyle(variant, lazy, accentColor, baseColor) };
 
   return (
     <>
