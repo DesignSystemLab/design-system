c867952888b689a25c1fcaf9e2acbfefd2397d54
jiyeon <jy7828@gmail.com>
2023-08-09 03:32:16 +0900

    🔧 modify: update storybook #81

--- packages/tabs/stories/tabs.stories.jsx
+++ packages/tabs/stories/tabs.stories.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Tabs } from '../src';
 
 export default {
@@ -35,7 +36,7 @@ export const Basic = () => (
 
 export const variant = () => (
   <>
-    <Tabs variant="underline">
+    <Tabs variant="underline" size="lg">
       <Tabs.List>
         <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
         <Tabs.Trigger value="tab2">tab2</Tabs.Trigger>
@@ -49,7 +50,21 @@ export const variant = () => (
       <Tabs.Content value="tab2">content2</Tabs.Content>
       <Tabs.Content value="tab3">content3</Tabs.Content>
     </Tabs>
-    <Tabs variant="enclosed">
+    <Tabs variant="enclosed" size="lg">
+      <Tabs.List>
+        <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
+        <Tabs.Trigger value="tab2">tab2</Tabs.Trigger>
+        <Tabs.Trigger value="tab3">tab3</Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="tab1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem ab, cum repellat perspiciatis distinctio
+        doloremque at sint beatae omnis sapiente reiciendis dicta quae veniam. Unde consequatur voluptates maxime
+        cupiditate!
+      </Tabs.Content>
+      <Tabs.Content value="tab2">content2</Tabs.Content>
+      <Tabs.Content value="tab3">content3</Tabs.Content>
+    </Tabs>
+    <Tabs variant="unstyled" size="lg">
       <Tabs.List>
         <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
         <Tabs.Trigger value="tab2">tab2</Tabs.Trigger>
@@ -109,7 +124,7 @@ export const onChange = () => {
 
 export const disabled = () => {
   return (
-    <Tabs variant="enclosed">
+    <Tabs variant="enclosed" baseColor="grey-lighten2">
       <Tabs.List>
         <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
         <Tabs.Trigger value="tab2" disabled>
@@ -144,6 +159,57 @@ export const lazy = () => {
       <Tabs.Content value="tab2">content2</Tabs.Content>
       <Tabs.Content value="tab3">content3</Tabs.Content>
     </Tabs>
+  );
+};
+
+export const color = () => {
+  return (
+    <>
+      <Tabs variant="enclosed" accentColor="primary-500" baseColor="primary-100">
+        <Tabs.List>
+          <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
+          <Tabs.Trigger value="tab2">tab2</Tabs.Trigger>
+          <Tabs.Trigger value="tab3">tab3</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="tab1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem ab, cum repellat perspiciatis distinctio
+          doloremque at sint beatae omnis sapiente reiciendis dicta quae veniam. Unde consequatur voluptates maxime
+          cupiditate!
+        </Tabs.Content>
+        <Tabs.Content value="tab2">content2</Tabs.Content>
+        <Tabs.Content value="tab3">content3</Tabs.Content>
+      </Tabs>
+      <br />
+      <Tabs variant="underline" accentColor="primary-500" baseColor="primary-100">
+        <Tabs.List>
+          <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
+          <Tabs.Trigger value="tab2">tab2</Tabs.Trigger>
+          <Tabs.Trigger value="tab3">tab3</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="tab1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem ab, cum repellat perspiciatis distinctio
+          doloremque at sint beatae omnis sapiente reiciendis dicta quae veniam. Unde consequatur voluptates maxime
+          cupiditate!
+        </Tabs.Content>
+        <Tabs.Content value="tab2">content2</Tabs.Content>
+        <Tabs.Content value="tab3">content3</Tabs.Content>
+      </Tabs>
+      <br />
+      <Tabs variant="unstyled" accentColor="primary-500" baseColor="primary-100">
+        <Tabs.List>
+          <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
+          <Tabs.Trigger value="tab2">tab2</Tabs.Trigger>
+          <Tabs.Trigger value="tab3">tab3</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="tab1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem ab, cum repellat perspiciatis distinctio
+          doloremque at sint beatae omnis sapiente reiciendis dicta quae veniam. Unde consequatur voluptates maxime
+          cupiditate!
+        </Tabs.Content>
+        <Tabs.Content value="tab2">content2</Tabs.Content>
+        <Tabs.Content value="tab3">content3</Tabs.Content>
+      </Tabs>
+    </>
   );
 };
 
@@ -226,6 +292,51 @@ export const size = () => {
       </Tabs>
       <br />
       <Tabs variant="underline" size="lg">
+        <Tabs.List>
+          <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
+          <Tabs.Trigger value="tab2">tab2</Tabs.Trigger>
+          <Tabs.Trigger value="tab3">tab3</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="tab1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem ab, cum repellat perspiciatis distinctio
+          doloremque at sint beatae omnis sapiente reiciendis dicta quae veniam. Unde consequatur voluptates maxime
+          cupiditate!
+        </Tabs.Content>
+        <Tabs.Content value="tab2">content2</Tabs.Content>
+        <Tabs.Content value="tab3">content3</Tabs.Content>
+      </Tabs>
+      <br />
+      <Tabs variant="unstyled" size="sm">
+        <Tabs.List>
+          <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
+          <Tabs.Trigger value="tab2">tab2</Tabs.Trigger>
+          <Tabs.Trigger value="tab3">tab3</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="tab1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem ab, cum repellat perspiciatis distinctio
+          doloremque at sint beatae omnis sapiente reiciendis dicta quae veniam. Unde consequatur voluptates maxime
+          cupiditate!
+        </Tabs.Content>
+        <Tabs.Content value="tab2">content2</Tabs.Content>
+        <Tabs.Content value="tab3">content3</Tabs.Content>
+      </Tabs>
+      <br />
+      <Tabs variant="unstyled" size="md">
+        <Tabs.List>
+          <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
+          <Tabs.Trigger value="tab2">tab2</Tabs.Trigger>
+          <Tabs.Trigger value="tab3">tab3</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="tab1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rem ab, cum repellat perspiciatis distinctio
+          doloremque at sint beatae omnis sapiente reiciendis dicta quae veniam. Unde consequatur voluptates maxime
+          cupiditate!
+        </Tabs.Content>
+        <Tabs.Content value="tab2">content2</Tabs.Content>
+        <Tabs.Content value="tab3">content3</Tabs.Content>
+      </Tabs>
+      <br />
+      <Tabs variant="unstyled" size="lg">
         <Tabs.List>
           <Tabs.Trigger value="tab1">tab1</Tabs.Trigger>
           <Tabs.Trigger value="tab2">tab2</Tabs.Trigger>
