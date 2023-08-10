6be20a4bba639d3c5c2911c4425aa255548f54e5
jiyeon <jy7828@gmail.com>
2023-08-11 04:41:45 +0900

    🧪 test: modify modal test #83

--- packages/modal/tests/modal.test.tsx
+++ packages/modal/tests/modal.test.tsx
@@ -11,22 +11,20 @@ import type { ModalProps } from '../src/types';
 expect.extend(toHaveNoViolations);
 
 const COMPONENT_DISPLAY_NAME_REGEX = /^[A-Z][A-Za-z]+(\.[A-Z][A-Za-z]+)*$/;
-const MODAL_TRIGGER_OPEN = 'modal-trigger-open';
-const MODAL_TRIGGER_CLOSE = 'modal-trigger-close';
+const MODAL_TRIGGER = 'modal-trigger';
+const MODAL_PORTAL = 'modal-portal';
 const onToggleModal = jest.fn();
 
 function renderModal(props: ModalProps) {
-  const { hasCloseIcon = false, open = false, lazy, ...restProps } = props;
+  const { hasCloseIcon = true, open = false, ...rest } = props;
 
   return render(
     <>
-      <Modal hasCloseIcon={hasCloseIcon} lazy={lazy} open={open} {...restProps}>
-        <Modal.Trigger data-testid={MODAL_TRIGGER_OPEN}>
+      <Modal hasCloseIcon={hasCloseIcon} open={open} {...rest}>
+        <Modal.Trigger data-testid={MODAL_TRIGGER}>
           <Button>트리거</Button>
         </Modal.Trigger>
-        <Modal.Header>헤더</Modal.Header>
-        <Modal.Body>바디</Modal.Body>
-        <Modal.Footer>푸터</Modal.Footer>
+        <Modal.Portal data-testid={MODAL_PORTAL}>모달 컨텐츠</Modal.Portal>
       </Modal>
     </>
   );
@@ -35,99 +33,50 @@ function renderModal(props: ModalProps) {
 describe('open and close modal correctly', () => {
   it('should not show modal before Trigger is clicked', () => {
     renderModal({});
-    const modalContent = document.querySelector('.modal_content');
-    expect(modalContent).not.toBeVisible();
+    const dialog = screen.queryByRole('dialog');
+    expect(dialog).not.toBeInTheDocument();
   });
-
   it('shows modal when Trigger is clicked', async () => {
     renderModal({});
+    const dialog = screen.queryByRole('dialog');
     const trigger = screen.getByRole('button');
-    await userEvent.click(trigger);
-    const modalContent = screen.getByRole('dialog');
-    expect(modalContent).toBeInTheDocument();
+    userEvent.click(trigger);
+    waitFor(() => {
+      expect(dialog).toBeVisible();
+    });
   });
-
-  it('fires onOpen when modal is opend', async () => {
+  it('fires onOpen when modal opens', async () => {
     renderModal({
       onOpen: onToggleModal
     });
-    const openTrigger = screen.getByRole('button');
-    await userEvent.click(openTrigger);
-    expect(onToggleModal).toHaveBeenCalled();
-  });
-
-  it('fires onClose when modal get closed', async () => {
-    renderModal({
-      onClose: onToggleModal
+    const trigger = screen.getByRole('button');
+    userEvent.click(trigger);
+    waitFor(() => {
+      expect(onToggleModal).toHaveBeenCalled();
     });
-    const Trigger = screen.getByRole('button');
-    await userEvent.click(Trigger);
-    const modalContent = screen.getByRole('dialog');
-    expect(modalContent).toBeInTheDocument();
-    await userEvent.click(Trigger);
-    expect(onToggleModal).toHaveBeenCalled();
   });
-
-  it('closes modal when <Trigger close> is clicked', async () => {
-    renderModal({});
-    const Trigger = screen.getByRole('button');
-    await userEvent.click(Trigger);
-    const modalContent = screen.getByRole('dialog');
-    expect(modalContent).toBeInTheDocument();
-    await userEvent.click(Trigger);
-    expect(modalContent).not.toBeInTheDocument();
-  });
-
-  it('closes modal when background is clikced', async () => {
+  it('closes modal when overlay is clicked', async () => {
     renderModal({
       open: true
     });
-    const modalContent = screen.getByRole('dialog');
-    const overlay = screen.getByRole('presentation');
-    await userEvent.click(overlay);
-    expect(modalContent).not.toBeInTheDocument();
-  });
-});
-
-describe('lazy', () => {
-  it('should not render items if it is lazy', () => {
-    renderModal({ lazy: true });
-    const modalContent = document.querySelector('.modal_content');
-    if (!modalContent) {
-      expect(true).toBe(true);
-    }
-  });
-
-  it('has already rendered modal content if it is NOT lazy', () => {
-    renderModal({ lazy: false });
-    const modalContent = document.querySelector('.modal_content');
-    if (modalContent) {
-      expect(modalContent).not.toBeVisible();
-    } else {
-      expect(true).toBe(false);
-    }
-  });
-});
-
-describe('close icon', () => {
-  it("has <Close> icon if it has 'hasCloseIcon' property", () => {
-    renderModal({ hasCloseIcon: true });
-    const closeIcon = document.querySelector('.header_icon');
-    const trigger = document.querySelector('modal_trigger');
-    userEvent.click(trigger!);
+    const dialog = screen.queryByRole('dialog');
+    const overlay = screen.queryByRole('none')!;
+    userEvent.click(overlay);
     waitFor(() => {
-      expect(closeIcon).toBeVisible();
+      expect(dialog).not.toBeInTheDocument();
     });
   });
-
-  it("doesn't have <Close> icon if it doesn't have 'hasCloseIcon' property", () => {
-    renderModal({});
-    const closeIcon = document.querySelector('.header_icon');
-    if (closeIcon) {
-      expect(true).toBe(false);
-    } else {
-      expect(true).toBe(true);
-    }
+  it('fires onClose when modal closes', async () => {
+    renderModal({
+      onClose: onToggleModal
+    });
+    const trigger = screen.getByRole('button');
+    const overlay = screen.queryByRole('none')!;
+    userEvent.click(trigger);
+    userEvent.click(overlay);
+    waitFor(() => {
+      expect(onToggleModal).toHaveBeenCalled();
+    });
   });
 });
 
@@ -135,7 +84,7 @@ describe('Modal', () => {
   behavesAsComponent({
     Component: Modal,
     toRender: () => {
-      <Modal></Modal>;
+      <Modal>Modal</Modal>;
     }
   });
 });
@@ -143,31 +92,15 @@ describe('Modal.Trigger', () => {
   behavesAsComponent({
     Component: Modal.Trigger,
     toRender: () => {
-      <Modal.Trigger></Modal.Trigger>;
+      <Modal.Trigger>Modal.Trigger</Modal.Trigger>;
     }
   });
 });
-describe('Modal.Header', () => {
+describe('Modal.Portal', () => {
   behavesAsComponent({
-    Component: Modal.Header,
+    Component: Modal.Portal,
     toRender: () => {
-      <Modal.Header></Modal.Header>;
-    }
-  });
-});
-describe('Modal.Body', () => {
-  behavesAsComponent({
-    Component: Modal.Body,
-    toRender: () => {
-      <Modal.Body></Modal.Body>;
-    }
-  });
-});
-describe('Modal.Footer', () => {
-  behavesAsComponent({
-    Component: Modal.Footer,
-    toRender: () => {
-      <Modal.Footer></Modal.Footer>;
+      <Modal.Portal>Modal.Portal</Modal.Portal>;
     }
   });
 });
